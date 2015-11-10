eea77b60584a05e97a016d90d543369cb55f0e6b
myteddy15 <lattedphilip@gmail.com>
2015-11-10 14:06:36 +0800

    Add function

--- controllers/students_controller.js
+++ controllers/students_controller.js
@@ -1,7 +1,21 @@
 "use strict";
 define(['app','api'], function (app) {
     app.register.controller('StudentController',['$scope','$rootScope','api', function ($scope,$rootScope,api) {
-       
+		$scope.init = function(){
+			$scope.Steps = [
+				{id:1, description:"Basic Information"},
+				{id:2, description:"Contact Information"},
+				{id:3, description:"Confirmation"}
+			];
+			$scope.ActiveStep=1;
+		};
+		$scope.nextStep = function(){
+			$scope.ActiveStep++;
+			console.log($scope.ActiveStep);
+		};
+		$scope.prevStep = function(){
+			$scope.ActiveStep--;
+		};
     }]);
 });
 
